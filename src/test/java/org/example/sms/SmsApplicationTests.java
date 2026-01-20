1420e1d127c2234b5cc47e816280aeec66c29449
Praveen Kumar <Praveenkumar042023@gmail.com>
2026-01-20 11:50:47 +0530

    setup model , service and controller package.

---
+++ src/test/java/org/example/sms/SmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
